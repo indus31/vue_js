d195fe8d30d6a65fdb9f0e259544bd334ec75138
indus31 <indus31@hotmail.fr>
2023-10-02 16:58:18 +0200

    save fin du 1er jour

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ Vue.createApp({
 //! Toutes les data seront contenues dans this
 data() {
     return {
+    nameUser: 'Dr.Mario',
+    ageUser: 45,
+    imageUser:'https://s3.amazonaws.com/medium.cosplay.com/77883/2111288.jpg',
+    compteur : 0,
     tasks: [],
     valeurDeInput: '',
-    toto:'bonjour'
+    toto:'bonjour',
+    texte : "hello world",
+    chiffres : 12354645,
+    tableaux : ["merci","aurevoir",9],
+    objet : {
+        nom:"mon nom",
+        prenom:"mon prenom",
+        tel:"0562258963"
+    },
+    htmlTitle: '<h1>Les mémoires de Steven Seagal</h1>',
+    Intrandom: Math.random() * 100
 };
 },
 //! Dans cet Object methods, on va écrire nos fonctions
 methods: {
+    agePlusDix(){
+        return this.ageUser + 10;
+    },
+    exemple(){
+        let randomInt = Math.random();
+        if(randomInt> 0.5){
+           return "blala";
+        }else{
+            return "titit";
+            
+        }
+    },
     ajouterTask() {
     this.tasks.push(this.valeurDeInput);
     this.valeurDeInput = '';
-    },
+    }
 },
 //! L'application est montée sur la balise HTML qui possède l'id app
 }).mount('#app');
--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-//On stock les éléments HTML dans des variables
-const leButton = document.querySelector('button');
-const leinput = document.querySelector('input');
-const laListe = document.querySelector('ul');
+Vue.createApp({
+    //! Dans une fonction data on va retourner un objet qui contiendra les variables de l'application
+    //! Toutes les data seront contenues dans this
+    data() {
+        return {
+        nombre: 0
+        
+    };
+    },
+    //! Dans cet Object methods, on va écrire nos fonctions
+    methods: {
+        
+        plusDix(param){
+            
+            return this.nombre += param;
+        },
+        moinsDix(param){
+            return this.nombre -= param;
 
-function ajouterTask() {
-    //Dans inputContent on stock ce que l'utilisateur tape dans l'input
-    const inputContent = leinput.value;
-    //Création d'un élément <li></li>
-    const listElement = document.createElement("li");
-    //Complétion du texte de l'élément <li></li>
-    listElement.textContent = inputContent;
-    //Ajout du <li></li> dans la <ul></ul> (laListe)
-    laListe.append(listElement);
-    //Bonus : après avoir ajouté une Task, on vide l'input
-    leinput.value = '';
-};
-leButton.addEventListener('click', ajouterTask);
-
-// Envoyer un message dans #vue
+        },
+        exemple(){
+            let randomInt = Math.random();
+            if(randomInt> 0.5){
+               return "blala";
+            }else{
+                return "titit";
+                
+            }
+        },
+        
+    },
+    //! L'application est montée sur la balise HTML qui possède l'id app
+    }).mount('#app');
